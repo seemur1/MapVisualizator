643d065c6884b29f200824574b315a8f3cf4f283
seemur1 <77749403+seemur1@users.noreply.github.com>
2023-05-13 16:45:21 +0300

    Update generator.py

--- GeneratorApp/generator.py
+++ GeneratorApp/generator.py
@@ -4,6 +4,15 @@ from queue import Queue
 from PIL import Image
 import sys
 
+
+def custom_rand(t):
+    return random.randint(0, t - 1) - random.randint(0, t - 1)
+
+
+def sign(num):
+    return -1 if num < 0 else 1
+
+
 # -----------------------------------------------------------------------------Классы--------------------------------------------------------------------------
 
 class Square:
@@ -278,209 +287,6 @@ class SquareMap:
 
 # --------------------------------------------------------------------------------Функции----------------------------------------------------------------------
 
-def custom_rand(t):
-    return random.randint(0, t - 1) - random.randint(0, t - 1)
-
-
-def sign(num):
-    return -1 if num < 0 else 1
-
-
-# Генерация одного континента
-def continent_generate(map, center_x, center_y, size_of_chunks_x, size_of_chunks_y, continent_size):
-    # Инициализация очереди обработки
-    under_development = Queue()
-    under_development.put((size_of_chunks_x * center_x, size_of_chunks_y * center_y))
-
-    # Инициализация счётчиков
-    size_of_generated_continent = 0
-    number_of_targets = 0
-
-    # Генерируем континент
-    while size_of_generated_continent < continent_size:
-        size_of_queue = under_development.qsize()
-
-        # Идём по всем обрабатываемым центрам генерации суши
-        for i in range(size_of_queue):
-
-            # Инициализация данных, используемых далее, и счётчиков
-            number_of_targets += 1
-            rand = random.randint(0, 100)
-            counter = 0
-
-            # Работаем с "лопающейся" клеткой
-            coords = under_development.get()
-            mates = map.pop(coords[0] // size_of_chunks_x, coords[1] // size_of_chunks_y, 1, size_of_chunks_x,
-                            size_of_chunks_y)
-
-            # Работа с соседями лопающейся клетки
-            for mate in mates:
-                square_mate = map.stock[mate[0]][mate[1]]
-                if square_mate.type == 1:
-                    counter += 1
-                if square_mate.turn == 0 and square_mate.count >= 3:
-                    under_development.put(mate)
-                    square_mate.turn = 1
-
-            # Работа непосредственно с лопающейся клуткой
-            if map.stock[coords[0]][coords[1]].type == 0:
-                if rand < counter * 28:
-                    map.continent_tipisation(coords[0], coords[1])
-
-        # Увеличиваем счётчик размера континента
-        size_of_generated_continent += 1
-
-
-# Генерация всех образов континентов
-def generate_map_of_continents(map, size_of_chunks_x, size_of_chunks_y, number_of_chunks_x, number_of_chunks_y,
-                               number_of_continents, size):
-    numof_generated_ccntinents = 0
-    while numof_generated_ccntinents < number_of_continents:
-
-        # Ищем центр генерируемого континента
-        rand_x = random.randint(1, number_of_chunks_x)
-        rand_y = random.randint(1, number_of_chunks_y)
-        if map.stock[size_of_chunks_x * rand_x][size_of_chunks_y * rand_y].typeres == 0:
-
-            # Инициализируем центр континента
-            map.initialize_center(size_of_chunks_x * rand_x, size_of_chunks_y * rand_y)
-
-            # Собираем прямых соседей выбранного центра генерации
-            mates = map.pop(rand_x, rand_y, 2, size_of_chunks_x, size_of_chunks_y)
-
-            # Работа с прямыми соседями
-            for mate in mates:
-                rand = random.randint(1, 50) + random.randint(1, 50)
-                # Заполняем соседей данными о посещаемости и типе биома
-                if rand < 85:
-                    map.continent_tipisation(mate[0], mate[1])
-
-            # Генерируем континент
-            continent_generate(map, rand_x, rand_y, size_of_chunks_x, size_of_chunks_y, size)
-
-            # Обнуляем все поля генерации для их дальнейшего заполнения
-            map.clear(size_of_chunks_x, size_of_chunks_y)
-
-            # Увеличиваем количество континентов
-            numof_generated_ccntinents += 1
-
-
-# Генерация рельефа в конкретном биоме
-def generate_chunk(map, center_x, center_y, radius, normal, t):
-    counter = 0
-    while counter < radius:
-        size_of_queue = map.under_development.qsize()
-
-        # Идём по "радиусу" лопнутых клеток
-        for n in range(size_of_queue):
-            # Узнаём, какую клетку сейчас нужно "лопнуть"
-            coords = map.under_development.get()
-            father = map.stock[coords[0]][coords[1]]
-
-            # Заполнение "лопающейся" клетки
-            map.popping_initialisation(coords, radius, counter)
-
-            # Лопаем клетку
-            mates = map.pop(coords[0], coords[1])
-
-            # Идём по соседям лопающейся клетки
-            for mate in mates:
-
-                square_mate = map.stock[mate[0]][mate[1]]
-
-                # Проверяем, не "лопнул" ли сосед клетки раньше?
-                if square_mate.turn == 0:
-
-                    # Увеличиваем высоту соседа "лопающейся" клетки
-                    square_mate.main += father.main
-
-                    # Если клетка получила данные с как минимум трёх соседей, то на "лопается" (добавляется к обрабатываемым)
-                    if square_mate.count >= 3:
-                        map.add_to_development(mate)
-
-                    # В зависимости от типа местности, передаём сосседям разную скорость изменения высоты
-                    map.speed_parameters_choosing(center_x, center_y, coords, mate, t, normal)
-
-            # Чистим "лопнутого" соседа
-            father.main = 0
-            father.speed = 0
-        counter += 1
-    # Чистим все системные значения, для следующих итераций.
-    map.clean_buffer()
-
-
-# Генерация рельефа на всей карте
-def generate_landscape(map, number_of_chunks_x, number_of_chunks_y, size_of_chunks_x, size_of_chunks_y, normal, t):
-    # Идём по всем чанкам
-    for i in range(1, number_of_chunks_x + 1):
-        for j in range(1, number_of_chunks_y + 1):
-            # Считаем расстояние, до которого будут генерироваться очаги генерации
-            radius = max((2 * (len(map.stock) // (number_of_chunks_x + 1)) + 8),
-                         (2 * (len(map.stock[0]) // (number_of_chunks_y + 1)) + 8))
-
-            center_x = i * size_of_chunks_x
-            center_y = j * size_of_chunks_y
-
-            # Игнициализируем центр чанка
-            map.biome_initialisation(center_x, center_y, normal, radius, t)
-
-            # Генерируем "чанк"
-            generate_chunk(map, center_x, center_y, radius, normal, t)
-
-
-# Cохранение массива высот в txt файле filename
-def write_map_array(stock, filename):
-    # Создаём отдельный файл filename.txt
-    with open('../ResultsDirectory/' + filename + '.txt', 'w') as file:
-
-        # Пишем размер карты первыми 2 цифрами в файл
-        file.write(str(len(stock)) + ' ' + str(len(stock[0])) + '\n')
-
-        output = ""
-        # Записываем каждую высоту в filename.txt
-        for x in range(len(stock)):
-            for y in range(len(stock[0])):
-                output += str(stock[x][y]) + " "
-            file.write(output + '\n')
-            output = ""
-
-
-# Генерация карты
-def map_generator(sizemap_x, sizemap_y, number_of_continents, filename, normal, t, size):
-    # Длина карты до аппроксимации
-    length = sizemap_x * 4
-    # Высота карты до аппроксимации
-    heigth = sizemap_y * 4
-    number_of_chunks_x = length // 25
-    number_of_chunks_y = heigth // 25
-
-    # Задаём необходимый размер карты
-    map = SquareMap(length, heigth, sizemap_x, sizemap_y)
-
-    size_of_chunks_x = length // (number_of_chunks_x + 1)
-    size_of_chunks_y = heigth // (number_of_chunks_y + 1)
-    numof_generated_ccntinents = 0
-
-    # Генерируем карту континентов
-    generate_map_of_continents(map, size_of_chunks_x, size_of_chunks_y, number_of_chunks_x, number_of_chunks_y,
-                               number_of_continents, size)
-
-    # Уведомляем об успешном размещении материков на образе карты
-    print("Generating_started")
-
-    # Генерируем высоты континента
-    generate_landscape(map, number_of_chunks_x, number_of_chunks_y, size_of_chunks_x, size_of_chunks_y, normal, t)
-
-    # Аппроксимация высот карты
-    map.heigths_approximation()
-
-    # Аппроксимация клеток карты
-    map.approximation()
-
-    write_map_array(map.interpolated_map, filename)
-
-    return map.interpolated_map
-
 
 # ---------------------------------------Тело программы---------------------------------------
 
@@ -491,7 +297,7 @@ class Generator:
     def __init__(self, x, y, cont_number, filename):
         borders = [15, 59, 71, 96]
         t, size_continent = 11, 20
-        self.array = map_generator(x, y, cont_number, filename, borders, t, size_continent)
+        self.array = self.map_generator(x, y, cont_number, filename, borders, t, size_continent)
 
         names = ['Water_deep.png', 'Water_not_deep.png', 'Plains.png', 'Forests.png', 'Mountains.png',
                  'Snowy_mountains.png']
@@ -507,7 +313,200 @@ class Generator:
             for y in range(len(self.array[0])):
                 self.image_out.paste(images[self.array[x][y]], (x * 41, y * 41))
         self.image_out.save('../ResultsDirectory/' + filename + ".png")
-        
+
+    # Генерация одного континента
+    def continent_generate(self, map, center_x, center_y, size_of_chunks_x, size_of_chunks_y, continent_size):
+        # Инициализация очереди обработки
+        under_development = Queue()
+        under_development.put((size_of_chunks_x * center_x, size_of_chunks_y * center_y))
+
+        # Инициализация счётчиков
+        size_of_generated_continent = 0
+        number_of_targets = 0
+
+        # Генерируем континент
+        while size_of_generated_continent < continent_size:
+            size_of_queue = under_development.qsize()
+
+            # Идём по всем обрабатываемым центрам генерации суши
+            for i in range(size_of_queue):
+
+                # Инициализация данных, используемых далее, и счётчиков
+                number_of_targets += 1
+                rand = random.randint(0, 100)
+                counter = 0
+
+                # Работаем с "лопающейся" клеткой
+                coords = under_development.get()
+                mates = map.pop(coords[0] // size_of_chunks_x, coords[1] // size_of_chunks_y, 1, size_of_chunks_x,
+                                size_of_chunks_y)
+
+                # Работа с соседями лопающейся клетки
+                for mate in mates:
+                    square_mate = map.stock[mate[0]][mate[1]]
+                    if square_mate.type == 1:
+                        counter += 1
+                    if square_mate.turn == 0 and square_mate.count >= 3:
+                        under_development.put(mate)
+                        square_mate.turn = 1
+
+                # Работа непосредственно с лопающейся клуткой
+                if map.stock[coords[0]][coords[1]].type == 0:
+                    if rand < counter * 28:
+                        map.continent_tipisation(coords[0], coords[1])
+
+            # Увеличиваем счётчик размера континента
+            size_of_generated_continent += 1
+
+    # Генерация всех образов континентов
+    def generate_map_of_continents(self, map, size_of_chunks_x, size_of_chunks_y, number_of_chunks_x,
+                                   number_of_chunks_y,
+                                   number_of_continents, size):
+        numof_generated_ccntinents = 0
+        while numof_generated_ccntinents < number_of_continents:
+
+            # Ищем центр генерируемого континента
+            rand_x = random.randint(1, number_of_chunks_x)
+            rand_y = random.randint(1, number_of_chunks_y)
+            if map.stock[size_of_chunks_x * rand_x][size_of_chunks_y * rand_y].typeres == 0:
+
+                # Инициализируем центр континента
+                map.initialize_center(size_of_chunks_x * rand_x, size_of_chunks_y * rand_y)
+
+                # Собираем прямых соседей выбранного центра генерации
+                mates = map.pop(rand_x, rand_y, 2, size_of_chunks_x, size_of_chunks_y)
+
+                # Работа с прямыми соседями
+                for mate in mates:
+                    rand = random.randint(1, 50) + random.randint(1, 50)
+                    # Заполняем соседей данными о посещаемости и типе биома
+                    if rand < 85:
+                        map.continent_tipisation(mate[0], mate[1])
+
+                # Генерируем континент
+                self.continent_generate(map, rand_x, rand_y, size_of_chunks_x, size_of_chunks_y, size)
+
+                # Обнуляем все поля генерации для их дальнейшего заполнения
+                map.clear(size_of_chunks_x, size_of_chunks_y)
+
+                # Увеличиваем количество континентов
+                numof_generated_ccntinents += 1
+
+    # Генерация рельефа в конкретном биоме
+    def generate_chunk(self, map, center_x, center_y, radius, normal, t):
+        counter = 0
+        while counter < radius:
+            size_of_queue = map.under_development.qsize()
+
+            # Идём по "радиусу" лопнутых клеток
+            for n in range(size_of_queue):
+                # Узнаём, какую клетку сейчас нужно "лопнуть"
+                coords = map.under_development.get()
+                father = map.stock[coords[0]][coords[1]]
+
+                # Заполнение "лопающейся" клетки
+                map.popping_initialisation(coords, radius, counter)
+
+                # Лопаем клетку
+                mates = map.pop(coords[0], coords[1])
+
+                # Идём по соседям лопающейся клетки
+                for mate in mates:
+
+                    square_mate = map.stock[mate[0]][mate[1]]
+
+                    # Проверяем, не "лопнул" ли сосед клетки раньше?
+                    if square_mate.turn == 0:
+
+                        # Увеличиваем высоту соседа "лопающейся" клетки
+                        square_mate.main += father.main
+
+                        # Если клетка получила данные с как минимум трёх соседей, то на "лопается" (добавляется к обрабатываемым)
+                        if square_mate.count >= 3:
+                            map.add_to_development(mate)
+
+                        # В зависимости от типа местности, передаём сосседям разную скорость изменения высоты
+                        map.speed_parameters_choosing(center_x, center_y, coords, mate, t, normal)
+
+                # Чистим "лопнутого" соседа
+                father.main = 0
+                father.speed = 0
+            counter += 1
+        # Чистим все системные значения, для следующих итераций.
+        map.clean_buffer()
+
+    # Генерация рельефа на всей карте
+    def generate_landscape(self, map, number_of_chunks_x, number_of_chunks_y, size_of_chunks_x, size_of_chunks_y,
+                           normal, t):
+        # Идём по всем чанкам
+        for i in range(1, number_of_chunks_x + 1):
+            for j in range(1, number_of_chunks_y + 1):
+                # Считаем расстояние, до которого будут генерироваться очаги генерации
+                radius = max((2 * (len(map.stock) // (number_of_chunks_x + 1)) + 8),
+                             (2 * (len(map.stock[0]) // (number_of_chunks_y + 1)) + 8))
+
+                center_x = i * size_of_chunks_x
+                center_y = j * size_of_chunks_y
+
+                # Игнициализируем центр чанка
+                map.biome_initialisation(center_x, center_y, normal, radius, t)
+
+                # Генерируем "чанк"
+                self.generate_chunk(map, center_x, center_y, radius, normal, t)
+
+    # Cохранение массива высот в txt файле filename
+    def write_map_array(self, stock, filename):
+        # Создаём отдельный файл filename.txt
+        with open('../ResultsDirectory/' + filename + '.txt', 'w') as file:
+
+            # Пишем размер карты первыми 2 цифрами в файл
+            file.write(str(len(stock)) + ' ' + str(len(stock[0])) + '\n')
+
+            output = ""
+            # Записываем каждую высоту в filename.txt
+            for x in range(len(stock)):
+                for y in range(len(stock[0])):
+                    output += str(stock[x][y]) + " "
+                file.write(output + '\n')
+                output = ""
+
+    # Генерация карты
+    def map_generator(self, sizemap_x, sizemap_y, number_of_continents, filename, normal, t, size):
+        # Длина карты до аппроксимации
+        length = sizemap_x * 4
+        # Высота карты до аппроксимации
+        heigth = sizemap_y * 4
+        number_of_chunks_x = length // 25
+        number_of_chunks_y = heigth // 25
+
+        # Задаём необходимый размер карты
+        map = SquareMap(length, heigth, sizemap_x, sizemap_y)
+
+        size_of_chunks_x = length // (number_of_chunks_x + 1)
+        size_of_chunks_y = heigth // (number_of_chunks_y + 1)
+        numof_generated_ccntinents = 0
+
+        # Генерируем карту континентов
+        self.generate_map_of_continents(map, size_of_chunks_x, size_of_chunks_y, number_of_chunks_x, number_of_chunks_y,
+                                        number_of_continents, size)
+
+        # Уведомляем об успешном размещении материков на образе карты
+        print("Generating_started")
+
+        # Генерируем высоты континента
+        self.generate_landscape(map, number_of_chunks_x, number_of_chunks_y, size_of_chunks_x, size_of_chunks_y, normal,
+                                t)
+
+        # Аппроксимация высот карты
+        map.heigths_approximation()
+
+        # Аппроксимация клеток карты
+        map.approximation()
+
+        self.write_map_array(map.interpolated_map, filename)
+
+        return map.interpolated_map
+
 
 if __name__ == '__main__':
     x, y, cnt, file = int(input()), int(input()), int(input()), input()
